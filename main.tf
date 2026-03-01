5e350d7b4a12f30c8039542f40fe783c821b4330
Vinnu0896 <vinodkumarkatakam8@gmail.com>
2026-03-02 02:22:11 +0530

    Initial clean commit

---
+++ main.tf
@@ -0,0 +1,81 @@
+############################################
+# VPC Module
+############################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "${var.cluster_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs = [
+    "${var.region}a",
+    "${var.region}b"
+  ]
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  public_subnets = [
+    "10.0.101.0/24",
+    "10.0.102.0/24"
+  ]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+############################################
+# EKS Module
+############################################
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.28"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns    = {}
+    kube-proxy = {}
+    vpc-cni    = {}
+  }
+
+  eks_managed_node_groups = {
+    general = {
+      instance_types = ["t3.medium"]
+
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
+
+      disk_size = 20
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
